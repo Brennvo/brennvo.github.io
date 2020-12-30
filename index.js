73f26de110d7336967f8ea8cac0efc95370d0bde
b-arvo <brennanarvo@gmail.com>
2020-12-29 20:50:59 -0600

    Site before restructure of HTML with nav

---
+++ index.js
@@ -0,0 +1,37 @@
+(function () {
+  setJsAvailableClass();
+  //hideMenu();
+  //setMenuDisplayStatus();
+  addMenuToggle();
+})();
+
+function setJsAvailableClass() {
+  document.body.classList.add("js");
+}
+
+// function setMenuDisplayStatus() {
+//   const menu = document.getElementsByClassName("menu__list");
+
+//   menu.classList.remove("hide");
+//   menu.classList.add("show");
+// }
+
+function hideMenu() {
+  const menuList = document.querySelector(".menu__list");
+  menuList.classList.add("hide");
+}
+
+function addMenuToggle() {
+  const menuBtn = document.querySelector(".menu__btn");
+
+  menuBtn.addEventListener("click", function () {
+    const list = this.nextElementSibling;
+    if (list.classList.contains("show")) {
+      this.setAttribute("aria-expanded", "true");
+      list.classList.remove("show");
+    } else {
+      this.setAttribute("aria-expanded", "true");
+      list.classList.add("show");
+    }
+  });
+}
---
+++ js/darkMode.js
@@ -0,0 +1,56 @@
+(function () {
+  initializeColorMode();
+  setColorModeToggleListener();
+})();
+
+function hasCookie(name) {
+  if (
+    document.cookie
+      .split(";")
+      .some((item) => item.trim().startsWith(`${name}=`))
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function hasCookieValue(name, value) {
+  if (
+    document.cookie.split(";").some((item) => item.includes(`${name}=${value}`))
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function initializeColorMode() {
+  if (hasCookie("mode") && hasCookieValue("mode", "dark")) {
+    document.body.classList.add("darkmode");
+  }
+}
+
+function resetDarkMode(oldVal) {
+  document.cookie = `mode=${oldVal}; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+}
+
+function setColorModeToggleListener() {
+  const toggleBtn = document.querySelector(".color-toggle");
+
+  if (toggleBtn) {
+    toggleBtn.addEventListener("click", function () {
+      if (hasCookie("mode") && hasCookieValue("mode", "dark")) {
+        resetDarkMode("dark");
+        document.cookie =
+          "mode=light; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+      } else {
+        resetDarkMode("light");
+        document.cookie =
+          "mode=dark; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+      }
+
+      document.body.classList.toggle("darkmode");
+    });
+  }
+}
